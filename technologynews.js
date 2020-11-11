4a512ccba1e1dd838217fee5546cd56509c13376
Ramesh-hash256 <65882296+Ramesh-hash256@users.noreply.github.com>
2020-11-11 11:29:35 +0530

    Add files via upload

--- technologynews.js
+++ technologynews.js
@@ -7,7 +7,7 @@ const xhr = new XMLHttpRequest();
 
 xhr.open(
   "GET",
-  "http://cors-anywhere.herokuapp.com/http://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=c5b24cbaeb2e4e88aed3e8471bd94218",
+  "https://cors-anywhere.herokuapp.com/newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=c5b24cbaeb2e4e88aed3e8471bd94218",
   true
 );
 
